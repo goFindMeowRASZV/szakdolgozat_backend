0d2a93300cc7234ca72decf170e41deced80b08a
radel01 <roszkopf.adel@gmail.com>
2025-01-21 10:30:15 +0100

    api

--- go_find_meow/routes/api.php
+++ go_find_meow/routes/api.php
@@ -1,8 +1,41 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\UserController;
+use App\Http\Middleware\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/users', [UserController::class, 'index']);
+
+Route::get('/user/{id}', [UserController::class, 'show']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+Route::post('/login', [AuthenticatedSessionController::class, 'store']); 
+
+//csrf
+Route::get('/sanctum/csrf-cookie',function(){
+    return response()-> json('CSRF token set');
+});
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+    //autentikált útvonal, user
+});
+
+
+Route::middleware(['auth:sanctum'])
+->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    // Kijelentkezés útvonal
+    Route::post('/logout', [AuthenticatedSessionController::class, 'destroy']);
+});
+
+//admin útvonal
+Route::middleware(['auth:sanctum', Admin::class])
+->group(function () {
+    Route::get('/admin/users', [UserController::class, 'index']);
 });
