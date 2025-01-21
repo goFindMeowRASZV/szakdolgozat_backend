f6c56cd086068c38f7e7183ca02a1e48dea44925
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2025-01-21 13:59:14 +0100

    back

--- go_find_meow/routes/api.php
+++ go_find_meow/routes/api.php
@@ -1,22 +1,25 @@
 <?php
 
-
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\Admin;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-/*Route::get('/users', [UserController::class, 'index']);
+Route::get('/users', [UserController::class, 'index']);
 
 Route::get('/user/{id}', [UserController::class, 'show']);
 Route::post('/register', [RegisteredUserController::class, 'store']);
-Route::post('/login', [AuthenticatedSessionController::class, 'store']);  */
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);  
 
 //csrf
-Route::get('/sanctum/csrf-cookie',function(){
+/* Route::get('/sanctum/csrf-cookie',function(){
     return response()-> json('CSRF token set');
 });
 
@@ -39,4 +42,33 @@ Route::middleware(['auth:sanctum'])
 Route::middleware(['auth:sanctum', Admin::class])
 ->group(function () {
     Route::get('/admin/users', [UserController::class, 'index']);
-});
+}); */
+
+
+Route::post('/register', [RegisteredUserController::class, 'store'])
+    ->middleware('guest')
+    ->name('register');
+
+Route::post('/login', [AuthenticatedSessionController::class, 'store'])
+    ->middleware('guest')
+    ->name('login');
+
+Route::post('/forgot-password', [PasswordResetLinkController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.email');
+
+Route::post('/reset-password', [NewPasswordController::class, 'store'])
+    ->middleware('guest')
+    ->name('password.store');
+
+Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
+    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->name('verification.verify');
+
+Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
+    ->middleware(['auth', 'throttle:6,1'])
+    ->name('verification.send');
+
+Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
+    ->middleware('auth')
+    ->name('logout');
