ed20bbdc5bb33f0b95396d3ad535325e082993e5
viktoriaszalkai <viktoriaszalkai04@gmail.com>
2025-01-28 14:31:21 +0100

    post report

--- goFindMeowBack/app/Http/Controllers/ShelteredCatController.php
+++ goFindMeowBack/app/Http/Controllers/ShelteredCatController.php
@@ -2,18 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ShelteredCat;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ShelteredCatController extends Controller
 {
-    //mentett macskak es kapcsolodo jelentesek
-     public function getShelteredCatsWithReports()
-     {
-         $data = DB::table('sheltered_cats as c')
-             ->select('c.cat_id', 'r.status', 'r.address', 'r.creator_id')
-             ->join('reports as r', 'c.report', '=', 'r.report_id')
-             ->get();
-         return $data;
-     }
+    public function index()
+    {
+        return ShelteredCat::all();
+    }
+    
+    public function store(Request $request)
+    {
+        $record = new ShelteredCat();
+        $record->fill($request->all());
+        $record->save();
+    }
+    
+    public function show(string $id)
+    {
+        return ShelteredCat::find($id);
+    }
+    
+    public function update(Request $request, string $id)
+    {
+        $record = ShelteredCat::find($id);
+        $record->fill($request->all());
+        $record->save();
+    }
+    
+    public function destroy(string $id)
+    {
+        ShelteredCat::find($id)->delete();
+    }
+    
 }
